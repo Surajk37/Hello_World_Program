10b740e3e5eae24db5d61fdc5587ab6e96ec9efc
Surajk37 <surajkhot9993@gmail.com>
2022-12-09 18:10:33 +0530

    [Suraj]:- Adding Hello World Program

---
+++ Hello_World_Program/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Hello_World_Program
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
+
